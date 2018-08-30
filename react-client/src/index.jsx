f13f41d9903b3a24375f137a2dd4cccf448dee5b
Himanshu Gaur <himanshugaur2610@gmail.com>
2018-08-30 18:28:22 -0500

    Completed basic login proces

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -6,14 +6,20 @@ import List from './components/List.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
-    }
+    this.state = {
+      items: [],
+      username: '',
+      password: ''
+    };
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
 
   componentDidMount() {
     $.ajax({
-      url: '/items', 
+      url: '/items',
       success: (data) => {
         this.setState({
           items: data
@@ -25,12 +31,62 @@ class App extends React.Component {
     });
   }
 
+  handleUsernameChange(event) {
+    this.setState({username: event.target.value});
+    console.log("username is:", this.state.username);
+  }
+
+  handlePasswordChange(event) {
+    this.setState({password: event.target.value});
+    console.log("password is:", this.state.password);
+  }
+
+  handleSubmit(event) {
+  console.log("inside search")
+    $.post('http://localhost:3000/items',
+    {username : this.state.username + ':'+this.state.password},
+    function(data, status) {
+      console.log('status', status);
+      console.log('data', data); });
+      this.setState({items: data});
+      // TODO
+    //alert('A user was submitted: ' + this.state.username);
+    //alert('A pw was submitted: ' + this.state.password);
+    //search(this.state.username,this.state.password);
+    //event.preventDefault();
+  }
+
+  search (uname) {
+  console.log("inside search")
+    $.post('http://localhost:3000/items',
+    {username : uname},
+    function(data, status) {
+      console.log('status', status);
+      console.log('data', data); });
+      this.setState({items: data});
+      // TODO
+    }
+
   render () {
     return (<div>
-      <h1>Item List</h1>
+      <h1>BillSplit</h1>
+      <h2> Login </h2>
       <List items={this.state.items}/>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          UserName:
+          <input type="text" username={this.state.value} onChange={this.handleUsernameChange} />
+        </label>
+        <label>
+          Password:
+          <input type="text" password={this.state.value} onChange={this.handlePasswordChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>)
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
