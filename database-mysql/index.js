2469b03478449e16cba3a637595478292ab5f8e7
Adam <rainbowfloyd911@gmail.com>
2018-09-01 14:55:50 -0400

    Added insert queries. Needs refactor to DRY

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -3,19 +3,20 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'FILL_ME_IN',
+  password : 'password',
   database : 'billSplit'
 });
 
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
-};
+connection.connect();
+
+// var selectAll = function(callback) {
+//   connection.query('SELECT * FROM items', function(err, results, fields) {
+//     if(err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, results);
+//     }
+//   });
+// };
 
 module.exports.connection = connection;
-module.exports.selectAll = selectAll;
