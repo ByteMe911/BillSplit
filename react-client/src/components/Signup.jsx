b655d96ad0980bef07a7991722d8292ac9a5ab89
Himanshu Gaur <himanshugaur2610@gmail.com>
2018-09-04 20:35:11 -0500

    Added the login completion and intial routing for signup

---
+++ react-client/src/components/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Signup = (props) => (
+  <div>
+    <h2>Sign up</h2>
+
+
+  </div>
+);
+
+
+
+
+export default Signup;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,37 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const db = require('../database-mysql/index.js')
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 var items = require('../database-mysql');
 // var items = require('../database-mongo');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // UNCOMMENT FOR REACT
 app.use(express.static(__dirname + '/../react-client/dist'));
 
+
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
+app.post('/items', function (req, res) {
+  console.log('Inside server req body',req.body);
+  var userNamePassword = req.body.username;
+  var userNamePasswordArray = userNamePassword.split(':');
+  console.log(userNamePasswordArray);
+  items.checkIfTheUserExists(userNamePasswordArray);
+  //items.insertUser(req.body);
+});
+
+app.post('/signup', function (req, res) {
+  console.log('Inside server req body',req.body);
+});
+
+app.get('/signup', function (req, res) {
+  console.log('Inside server req body',req.body);
+});
 
 app.get('/items', function (req, res) {
   items.selectAll(function(err, data) {
